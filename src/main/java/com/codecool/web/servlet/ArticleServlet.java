8f71c1c8e6f459802b6c3090667d16845791018f
Zoltan <zolee95@gmail.com>
2019-03-13 11:44:24 +0100

    add text page

---
+++ src/main/java/com/codecool/web/servlet/ArticleServlet.java
@@ -0,0 +1,25 @@
+package com.codecool.web.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/add_article")
+public class ArticleServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        String title = req.getParameter("article_title");
+        String content = req.getParameter("content");
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("add_article.html").forward(req, resp);
+    }
+}
--- src/main/webapp/content.jsp
+++ src/main/webapp/content.jsp
@@ -30,6 +30,7 @@
         %>
         <a class="button" href="users">Users</a>
         <a class="button" href="profile">Profile</a>
+        <a class="button" href="add_article">Add Article</a>
         <%
         Article article = (Article) request.getAttribute("article");
         %>
