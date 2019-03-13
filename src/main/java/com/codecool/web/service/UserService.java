c80a7c61da0e6579becffbe80b577736094083be
szilardGaal <gaalszilard23@gmail.com>
2019-03-13 11:05:28 +0100

    refactoring in database and userservlet. minimal css changes

--- src/main/java/com/codecool/web/service/UserService.java
+++ src/main/java/com/codecool/web/service/UserService.java
@@ -2,37 +2,31 @@ package com.codecool.web.service;
 
 import com.codecool.web.model.User;
 
+import java.io.DataOutputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 public final class UserService {
 
-    public List<User> getUsers() {
-        Database database = Database.getInstance();
-        return database.users;
+    Database database = Database.getInstance();
+
+    public User[] getUsers() {
+        return database.getUsersArray();
     }
 
     public void addUser(String username, String password, String email) {
-        Database database = Database.getInstance();
-        database.users.add(new User(username, password, email));
+        database.addUser(new User(username, password, email));
     }
 
     public boolean validateLogIn(String username, String password) {
-        Database database = Database.getInstance();
-        for (User u : database.users) {
-            if (u.getUsername().equals(username) && u.getPassword().equals(password)) {
-                return true;
-            }
+        User u = database.getUserByName(username);
+        if (u.getPassword().equals(password)) {
+            return true;
         }
         return false;
     }
 
     public boolean validateRegistration(String username) {
-        Database database = Database.getInstance();
-        for (User u : database.users) {
-            if (u.getUsername().equals(username)) {
-                return false;
-            }
-        }
-        return true;
+        return !database.getUserNames().contains(username);
     }
 }
