225d38d252461cb45b0c42bfa1a1b31654700838
szilardGaal <gaalszilard23@gmail.com>
2019-03-27 10:57:08 +0100

    works on attendance

--- src/main/java/com/codecool/web/servlet/AttendanceServlet.java
+++ src/main/java/com/codecool/web/servlet/AttendanceServlet.java
@@ -10,23 +10,37 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @WebServlet("/attendance")
 public class AttendanceServlet extends HttpServlet {
     
     private Database database = MockDatabase.getInstance();
+    private User[] users = database.getUsersArray();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        User[] users = database.getUsersArray();
         req.setAttribute("users", users);
         req.getRequestDispatcher("attendance.jsp").forward(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        
+        String dateString = req.getParameter("date");
 
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
 
+        try {
+            Date date = df.parse(dateString);
+            for (User u : users) {
+                u.setAttendance(date, Boolean.valueOf(req.getParameter(u.getUsername())));
+            }
+        } catch (ParseException e) {
+            req.setAttribute("errorMessage", "Invalid date!");
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        }
     }
 }
