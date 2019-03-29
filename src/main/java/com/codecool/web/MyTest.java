636949ff8ddf6f7186890da5fa4215e9fcb2de43
Szűcs Áron <aron.szucs.miskolc@gmail.com>
2019-03-29 09:07:33 +0100

    upgoating

--- src/main/java/com/codecool/web/MyTest.java
+++ src/main/java/com/codecool/web/MyTest.java
@@ -5,6 +5,7 @@ import com.codecool.web.model.Content;
 import com.codecool.web.service.Database;
 import com.codecool.web.service.DatabaseLoader;
 import com.codecool.web.service.MockDatabase;
+import com.codecool.web.service.SqlGenerator;
 
 import java.io.IOException;
 import java.util.List;
@@ -14,12 +15,8 @@ public class MyTest {
         DatabaseLoader databaseLoader = new DatabaseLoader();
 
         Content content = databaseLoader.loadContent("src/main/webapp/articles.xml", "src/main/webapp/quizzes.xml");
-        Database db = new MockDatabase(content);
-        List<Integer> kecske = db.getQuizIdsByLevel(4);
-
-        for (int n : kecske) {
-            System.out.println(n);
-        }
+        SqlGenerator sqlGenerator = new SqlGenerator(content);
+        System.out.println(sqlGenerator.generate());
 
     }
 }
