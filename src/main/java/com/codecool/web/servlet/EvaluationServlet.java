9162b1e442bdccc02a23d0645b8e24688415fcf4
Szűcs Áron <aron.szucs.miskolc@gmail.com>
2019-04-09 10:53:50 +0200

    again, before pull

--- src/main/java/com/codecool/web/servlet/EvaluationServlet.java
+++ src/main/java/com/codecool/web/servlet/EvaluationServlet.java
@@ -1,5 +1,6 @@
 package com.codecool.web.servlet;
 
+import com.codecool.web.dao.UserDao;
 import com.codecool.web.model.NoSuchUserException;
 import com.codecool.web.model.User;
 import com.codecool.web.service.UserService;
@@ -8,22 +9,35 @@ import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 import java.io.IOException;
+import java.sql.SQLException;
 
 @WebServlet("/evaluate")
 public class EvaluationServlet extends AbstractServlet {
 
-    private final UserService service = new UserService();
+    private UserService service;
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        User user = (User) req.getAttribute("user");
-        int quizID = (int) req.getAttribute("id");
-        //Solution solution = solutionService.getSolution(user.getEmail(), quizID);
-        
+        try (UserDao userDao = new UserDao(getConnection(req.getServletContext()))) {
+            service = new UserService(userDao);
+            User user = (User) req.getAttribute("user");
+            int quizID = (int) req.getAttribute("id");
+            //Solution solution = solutionService.getSolution(user.getEmail(), quizID);
+        } catch (SQLException e) {
+            handleError(e, req, resp);
+        }
+
+
 
     }
 
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try (UserDao userDao = new UserDao(getConnection(req.getServletContext()))) {
+            service = new UserService(userDao);
+            // Logic goes here
+        } catch (SQLException e) {
+            handleError(e, req, resp);
+        }
 
 
     }
